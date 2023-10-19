4c4c84444e11f2a14b216fef96d13f3a3180e345
nikolai.tatarinov <nikolai.tatarinov@soundcloud.com>
2023-10-19 20:21:34 +0200

    fix logging; more next.jdbc examples

--- src/web_app/dev.clj
+++ src/web_app/dev.clj
@@ -10,14 +10,17 @@
   (def ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "example"}))
   (def ds-opts (jdbc/with-options ds {:builder-fn rs/as-unqualified-kebab-maps}))
 
+  (jdbc/execute! ds ["drop table address"])
   (jdbc/execute! ds ["create table address (id INTEGER PRIMARY KEY, name varchar(32), last_name varchar(32))"])
 
   (jdbc/execute! ds ["insert into address (name, last_name) values('Jesse', 'Faden')"])
-  (jdbc/execute! ds ["insert into address (name, last_name) values('Alan', 'Wake')"])
+  (sql/insert! ds :address {:name "Alan" :last_name "Wake"})
 
   (sql/query ds-opts ["select * from address"])
+  (sql/query ds-opts ["select count(*) as cnt from address"])
 
   (sql/find-by-keys ds-opts :address {:name "Jesse"})
+  (sql/find-by-keys ds-opts :address ["name = ?", "Jesse"])
   (def address (jdbc/execute-one! ds-opts ["select * from address where name = ?", "Jesse"]))
 
   (:id address)
